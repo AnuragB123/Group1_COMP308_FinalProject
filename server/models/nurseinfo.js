b86fb4378b80992aa084cfd1d6109c4fb2495148
AnuragB123 <52175974+AnuragB123@users.noreply.github.com>
2022-04-07 15:18:28 -0400

    Nurse Model

--- server/models/nurseinfo.js
+++ server/models/nurseinfo.js
@@ -2,3 +2,51 @@
 //body temperature, heart rate, blood pressure (Create and Read), Daily Motivation Tips
 //Reference to Patient
 //Prajwal and Anurag
+
+import mongoose from 'mongoose';
+
+let nurseSchema = new mongoose.Schema({
+    nurseId: {
+        type: Number,
+        default: '',
+        trim: true,
+        required: true
+    },
+    bodyTemperature: {
+        type: Number,
+        default: '',
+        trim: true,
+        required: true
+    },
+    heartRate: {
+        type: Number,
+        default: '',
+        trim: true,
+        required: true
+    },
+    bloodPressure: {
+        type: Number,
+        default: '',
+        trim: true,
+        required: true
+    },
+    resporitoryRate: {
+        type: Number,
+        default: '',
+        trim: true,
+        required: true
+    },
+    dailyMotivationTips: {
+        type: String,
+        default: '',
+        trim: true,
+        required: true
+    }
+},
+{
+    collection: 'Nurses'
+});
+
+let Nurse = mongoose.model('Nurse', nurseSchema);
+
+export default Nurse;
