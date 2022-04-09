1f7c5d70cd49d17569f00021a902b5deca528001
AnuragB123 <52175974+AnuragB123@users.noreply.github.com>
2022-04-09 18:06:11 -0400

    Create nurseResolver.js
    NurseResolver js GraphQL for Nurse Model

---
+++ server/setup/resolver/nurseResolver.js
@@ -0,0 +1,29 @@
+import Nurse from '../../models/nurseinfo'
+
+export default {
+    nurse: async (res,req, nurseId) => {
+        const nurseinfo = await Nurse.findOne({_id: nurseId});
+        if (!nurseinfo) {
+            throw new Error('Error')
+        }
+        return nurseinfo
+    },
+    createNurse: async (args) => {
+        try {
+            const existingNurse = await Nurse.findOne({ nurseId: args.userInput.nurseId});
+            if (existingNurse) {
+                throw new Error('Nurse exists already');
+            }
+            const nurse = new Nurse({
+                nurseId: args.userInput.nurseId,
+                bodyTemperature: args.userInput.bodyTemperature,
+                heartRate: args.userInput.heartRate,
+                bloodPressure: args.user.bloodPressure,
+                resporitoryRate: args.user.resporitoryRate,
+                dailyMotivationTips: args.user.dailyMotivationTips
+            })
+        } catch (err) {
+            throw err;
+        }
+    }
+}
