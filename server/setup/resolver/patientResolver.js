259109d1379333902893f1d0a637a0a4a3fb8d8f
pregmi1 <77637021+pregmi1@users.noreply.github.com>
2022-04-20 12:07:09 -0400

    updated

--- server/setup/resolver/patientResolver.js
+++ server/setup/resolver/patientResolver.js
@@ -1,65 +1,68 @@
 import Patient from '../../models/patientinfo.js';
 
 export default {
-  patient: async (res, req, patientId) => {
-    const patientinfo = await Patient.findOne({_id: patientId});
-    if (!patientinfo) {
-            throw new Error('Error')
-        }
-    return patientinfo;
-  },
-  createpatient: async (args, req, res) => {
-    try {
-        const existingalert = await Alert.findOne({ patientId: args.userInput.patientId});
-        if (existingpatient) {
-            throw new Error('Patient exists already');
-        }
-        const patient = new Patient({
-            patientId: args.userInput.patientId,
-            patientName: args.userInput.patientName,
-            bodyTemperature: args.userInput.bodyTemperature,
-            heartRate: args.userInput.heartRate,
-            bloodPressure: args.userInput.bloodPressure,
-            resporitoryRate: args.userInput.resporitoryRate,
-            signsSymptoms: args.userInput.signsSymptoms,
-            nurse: req.nurse,
-            emergencyAlert: req.emergencyAlert,
-        });
-      patient.save().then(() => {
-          return res.status(200).json({
+    patients: async () => {
+        const patientinfo = await Patient.find();
+        if (!patientinfo) {
+                throw new Error('Error')
+            }
+        return patientinfo;
+    },
+    patient: async (res, req, patientId) => {
+        const patientinfo = await Patient.findOne({_id: patientId});
+        if (!patientinfo) {
+                throw new Error('Error')
+            }
+        return patientinfo;
+    },
+    createpatient: async (args, req, res) => {
+        try {
+            const existingpatient = await Alert.findOne({ patientId: args.userInput.patientId});
+            if (existingpatient) {
+                throw new Error('Patient exists already');
+            }
+            const patient = new Patient({
+                patientId: args.userInput.patientId,
+                patientName: args.userInput.patientName,
+                bodyTemperature: args.userInput.bodyTemperature,
+                heartRate: args.userInput.heartRate,
+                bloodPressure: args.userInput.bloodPressure,
+                resporitoryRate: args.userInput.resporitoryRate,
+                signsSymptoms: args.userInput.signsSymptoms,
+                nurse: req.nurse,
+                emergencyAlert: req.emergencyAlert,
+            });
+            return res.status(200).json({
                 success: true,
                 message: 'Patient Add!',
-         })
-      });
-     } catch (err) {
-         throw err;
-     }
-  },
-  updatepatient: async (args, req, res) => {
-    try {
-        const existingalert = await Alert.findOne({ patientId: args.userInput.patientId});
-        if (!existingpatient) {
-            throw new Error('Patient does not exists');
+            });
+        } catch (err) {
+            throw err;
         }
-        const patient = new Patient({
-            patientId: args.userInput.patientId,
-            patientName: args.userInput.patientName,
-            bodyTemperature: args.userInput.bodyTemperature,
-            heartRate: args.userInput.heartRate,
-            bloodPressure: args.userInput.bloodPressure,
-            resporitoryRate: args.userInput.resporitoryRate,
-            signsSymptoms: args.userInput.signsSymptoms,
-            nurse: req.nurse,
-            emergencyAlert: req.emergencyAlert,
-        });
-        patient.save().then(() => {
-          return res.status(200).json({
+    },
+    updatepatient: async (args, req, res) => {
+        try {
+            const existingpatient = await Alert.findOne({ patientId: args.userInput.patientId});
+            if (!existingpatient) {
+                throw new Error('Patient does not exists');
+            }
+            const patient = new Patient({
+                patientId: args.userInput.patientId,
+                patientName: args.userInput.patientName,
+                bodyTemperature: args.userInput.bodyTemperature,
+                heartRate: args.userInput.heartRate,
+                bloodPressure: args.userInput.bloodPressure,
+                resporitoryRate: args.userInput.resporitoryRate,
+                signsSymptoms: args.userInput.signsSymptoms,
+                nurse: req.nurse,
+                emergencyAlert: req.emergencyAlert,
+            });
+            return res.status(200).json({
                 success: true,
                 message: 'Patient Updated!',
-           })
-        });
-     } catch (err) {
-         throw err;
-     }
-   }
+            });
+        } catch (err) {
+            throw err;
+        }
+    }
 }
