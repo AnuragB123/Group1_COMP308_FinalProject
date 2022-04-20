21f2c653a16f76e3e884051e67fdc3bbe2301fa3
AnuragB123 <52175974+AnuragB123@users.noreply.github.com>
2022-04-19 20:53:52 -0400

    Create Nurse and extra packages

---
+++ frontend/src/pages/CreateNurse.js
@@ -0,0 +1,106 @@
+//Import libraries
+import React, { Component } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import { gql, useMutation } from '@apollo/client';
+import { Link} from 'react-router-dom';
+
+//GraphQL Mutation to Create a Nurse
+const CREATENURSE = gql`
+mutation CreateNurse(
+    $nurseId: Int!,
+    $bodyTemperature: Int!,
+    heartRate: Int!,
+    bloodPressure: Int!,
+    resporitoryRate: Int!,
+    dailyMotivationTips: String!,
+) {
+    createNurse(
+        nurseId: $nurseId,
+        bodyTemperature: $bodyTemperature,
+        heartRate: $heartRate,
+        bloodPressure: $bloodPressure,
+        resporitoryRate: $resporitoryRate,
+        dailyMotivationTips: $dailyMotivationTips
+        ) {
+            nurseId
+        }
+}
+`;
+
+//Form to create a Nurse using GraphQL
+const CreateNurse = () => {
+    let nurseId, bodyTemperature, heartRate, bloodPressure, resporitoryRate, dailyMotivationTips;
+    const [createNurse, {data, loading, error}] = useMutation(createNurse);
+    if (loading) return 'Submitting...';
+    if (error) return `Submission error! ${error.message}`;
+    return (
+        <div>
+            <Jumbotron>
+                <form onSubmit={e => {    
+                    e.preventDefault();
+                    createNurse({ variables: { nurseId: nurseId.value, bodyTemperature: bodyTemperature.value, 
+                        heartRate: heartRate.value, bloodPressure: bloodPressure.value, resporitoryRate: resporitoryRate.value, 
+                        dailyMotivationTips: dailyMotivationTips.value
+                      } });
+                      nurseId.value = '';
+                      bodyTemperature.value = '';
+                      heartRate.value = '';
+                      bloodPressure.value = '';
+                      resporitoryRate.value = '';
+                      dailyMotivationTips.value = '';
+                }}
+                >
+                <div class="outer_container">
+                <div class="container">
+                    <label>
+                        <b>Nurse ID:</b>
+                    </label>
+                    <input type="number" class="fields" name="nurseID" ref={node => {nurseId = node; }} 
+                    placeholder="Nurse ID:" />
+                </div>
+                <div class="container">
+                    <label>
+                        <b>Body Temperature:</b>
+                    </label>
+                    <input type="number" class="fields" name="bodyTemperature" ref={node => {bodyTemperature = node; }} 
+                    placeholder="Body Temperature:" />
+                </div>
+                <div class="container">
+                    <label>
+                        <b>Heart Rate:</b>
+                    </label>
+                    <input type="number" class="fields" name="heartRate" ref={node => {heartRate = node; }}
+                    placeholder="Heart Rate:" />
+                </div>
+                <div class="container">
+                    <label>
+                        <b>Blood Pressure:</b>
+                    </label>
+                    <input type="number" class="fields" name="bloodPressure" ref={node => {bloodPressure = node; }}
+                    placeholder="Blood Pressure:" />
+                </div>
+                <div class="container">
+                    <label>
+                        <b>Resporitory Rate:</b>
+                    </label>
+                    <input type="number" class="fields" name="resporitoryRate" ref={node => {resporitoryRate = node; }}
+                    placeholder="Resporitory Rate:" />
+                </div>
+                <div class="container">
+                    <label>
+                        <b>Daily Motivation Tips:</b>
+                    </label>
+                    <input type="text" class="fields" name="dailyMotivationTips" ref={node => {dailyMotivationTips = node; }}
+                    placeholder="Daily Motivation Tips:" />
+                </div>
+                <div class="container">
+                    <button type="submit" class="fields">Save Nurse</button>
+                </div>
+                </div>
+            </form>
+            </Jumbotron>
+        </div>
+    );
+}
+export default CreateNurse
--- frontend/src/pages/Nurse.js
+++
@@ -1,3 +0,0 @@
-//This page should allow a Nurse User create and store the vital information
-//Another page to view the information Nurse inputs
-//Another page to send just the Daily motivational tips
