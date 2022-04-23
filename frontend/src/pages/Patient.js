b3018e0ac50f19d87a106f503cbfad9a25e29fcf
pregmi1 <77637021+pregmi1@users.noreply.github.com>
2022-04-22 20:11:49 -0400

    Add files via upload

--- frontend/src/pages/Patient.js
+++ frontend/src/pages/Patient.js
@@ -1,50 +1,39 @@
-/*
-Group 1: Anurag Bhattacharya, Aaishi Sinha, Illah Song, Joseph Volpe, Prajwal Regmi
-*/
-
-/*
-In this Page, a Patient User would be able to input their vital signs
-Prajwal and Illah developed this
- */
+//Page to create and send emergency alert messages
+//Page to enter daily information
+//Page to view daily motivational tips
+//Page to enter checklist of common signs and symptoms and ask a question about and get answer (ML)
 
 //Import libraries
 import React from 'react';
-import { gql, useQuery, useMutation } from "@apollo/client";
-import { Form, Container, Row, Col, Checkbox } from 'react-bootstrap';
+import { gql, useQuery } from "@apollo/client";
+import NewPatient from './createPatient';
 
 //GraphQL Query to get Patient based on ID
 const PATIENT = gql`
-{
-    query($id: String) {
-        patient(patientId: $patientId)(
-            patientId,
-            patientName,
-            bodyTemperature,
-            heartRate,
-            bloodPressure,
-            resporitoryRate,
-            dailyMotivationTips
-        ) {
+    query Patient($patientId: String!) {
+        patient(patientId: $patientId){
             patientId
+            patientName
+            bodyTemperature
+            heartRate
+            bloodPressure
+            resporitoryRate
+            signsSymptoms
         }
     }
-}
 `;
-
-const CREATEPATIENT = gql``;
 
 //Page to show a Patient
 const PatientList = () => {
     const { loading, error, data, refetch } = useQuery(PATIENT);
-
     if (loading) return <p>Loading...</p>;
-    if (error) return <p> Error:(</p>;
+    if (error) return <p> <NewPatient/></p>;
 
     return (
         <div>
             {(data)?(
                 <div>
-                    <p style={padding="16px"}>
+                    <p>
                         <b>Patient Name: {data.patients.patientName}</b>
                         <b>Patient ID: {data.patients.patientId}</b>
                     </p>
@@ -73,44 +62,13 @@ const PatientList = () => {
                         <button class="center" onClick={() => refetch()}>Refetch</button>
                     </div>  
                     <div>                
-                    <Form>
-                        <Row className="mb-3">
-                            <Form.Group as={Col}>
-                                <Form.Label>Body Temperature: </Form.Label>
-                                <Form.Control type="number" id="bodyTemperature" value='0' name="bodyTemperature" ref={node => {bodyTemperature = node;}} />
-                            </Form.Group>
-                            <Form.Group as={Col}>
-                                <Form.Label>Heart Rate: </Form.Label>
-                                <Form.Control type="number" id="heartRate" value='0' name="heartRate" ref={node => {heartRate = node;}} />
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-3">
-                            <Form.Group as={Col}>
-                                <Form.Label>Blood Pressure: </Form.Label>
-                                <Form.Control type="number" id="bloodPressure" value='0' name="bloodPressure" ref={node => {bloodPressure = node;}} />
-                            </Form.Group>
-                            <Form.Group as={Col}>
-                                <Form.Label>Resporitory Rate: </Form.Label>
-                                <Form.Control type="number" id="resporitoryRate" value='0' name="resporitoryRate" ref={node => {resporitoryRate = node;}} />
-                            </Form.Group>
-                        </Row>
-                        <Form.Group as={Col}>
-                            <Form.Label>Signs &#38; Symptoms: </Form.Label> 
-                            <Form.Check inline label="Runny nose" value='Runny nose' name="signsSymptoms" type={'checkbox'} id="signsSymptoms"/>
-                            <Form.Check inline label="Sinus pain from congestion" value="Sinus pain from congestion" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" />
-                            <Form.Check inline label="Spots and blisters" value="Spots and blisters" type={'checkbox'} id="signsSymptoms" />
-                            <Form.Check inline label="Poor wound healing" value="Poor wound healing"name="signsSymptoms" type={'checkbox'} id="signsSymptoms" />
-                            <Form.Check inline label="Thirst" value="Thirst" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" />
-                            <Form.Check inline label="Fast heart rate" value="Fast heart rate" type={'checkbox'} id="signsSymptoms" />
-                            <Form.Check inline label="Chest Pain" value="Chest Pain" type={'checkbox'} id="signsSymptoms" />
-                            <Form.Check inline label="Fever" value="Fever" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" />
-                            <Form.Check inline label="Feelings of misery" value="Feelings of misery" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" />   
-                        </Form.Group>
-                    </Form>
+                        <NewPatient/>
                     </div>
                 </div>
                 ):(
-                    <p style={padding="16px"}><b>Loading....</b></p>
+                    <div>                                   
+                        <p>Error :(</p>
+                    </div>
                 )
             }                         
         </div>
---
+++ frontend/src/pages/createPatient.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { Form, Row, Col } from 'react-bootstrap';
+
+const CREATEPATIENT = gql`
+    mutation CreatePatient(
+        $patientId: String!,
+        $patientName:String!,
+        $bodyTemperature: Int!,
+        $heartRate: Int!,
+        $bloodPressure: Int!,
+        $resporitoryRate: Int!,
+        $signsSymptoms: String!,
+    ) {
+        createPatient( patientInput:{
+            patientId: $patientId,
+            patientName: $patientName,
+            bodyTemperature: $bodyTemperature,
+            heartRate: $heartRate,
+            bloodPressure: $bloodPressure,
+            resporitoryRate: $resporitoryRate,
+            signsSymptoms: $signsSymptoms
+        }) {
+                patientId
+                patientName
+            }
+    }
+`;
+
+const NewPatient = (props) => {
+    let patientId, patientName, bodyTemperature, heartRate, bloodPressure, resporitoryRate, signsSymptoms;
+    const [createPatient, {data, loading, error}] = useMutation(CREATEPATIENT);
+    if (loading) return 'Submitting...';
+    if (error) return `Submission error! ${error.message}`;
+    return(
+        <div>
+            {(!props.data)?(        
+            <Form onSubmit={e=>{
+                e.preventDefault();
+                createPatient({variables: {
+                    patientId: patientId.value,
+                    patientName: patientName.value,
+                    bodyTemperature: bodyTemperature.value,
+                    heartRate: heartRate.value,
+                    bloodPressure: bloodPressure.value,
+                    resporitoryRate: resporitoryRate.value,
+                    signsSymptoms: signsSymptoms.value
+                }})
+            }}>
+                <p><b>Add Patient</b></p>   
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Patient ID: </Form.Label>
+                        <Form.Control type="text" id="patientId" disabled value={`P${Math.floor(Math.random() * 1000)}`} name="patientId" ref={node => {patientId = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Patient Name: </Form.Label>
+                        <Form.Control type="text" id="patientName" placeholder="Name" value='Name' name="patientName" ref={node => {patientName = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Body Temperature: </Form.Label>
+                        <Form.Control type="number" id="bodyTemperature" value='0' name="bodyTemperature" ref={node => {bodyTemperature = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Heart Rate: </Form.Label>
+                        <Form.Control type="number" id="heartRate" value='0' name="heartRate" ref={node => {heartRate = node}} />
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Blood Pressure: </Form.Label>
+                        <Form.Control type="number" id="bloodPressure" value='0' name="bloodPressure" ref={node => {bloodPressure = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Resporitory Rate: </Form.Label>
+                        <Form.Control type="number" id="resporitoryRate" value='0' name="resporitoryRate" ref={node => {resporitoryRate = node}} />
+                    </Form.Group>
+                </Row>
+                <Form.Group as={Col}>
+                    <Form.Label>Signs &#38; Symptoms: </Form.Label> 
+                    <Form.Check inline label="Runny nose" value='Runny nose' name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Sinus pain from congestion" value="Sinus pain from congestion" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Spots and blisters" value="Spots and blisters" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Poor wound healing" value="Poor wound healing"name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Thirst" value="Thirst" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Fast heart rate" value="Fast heart rate" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Chest Pain" value="Chest Pain" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Fever" value="Fever" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Feelings of misery" value="Feelings of misery" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />   
+                </Form.Group>
+                <button type="submit" > Confirm</button>
+            </Form>
+            ):(
+                <Form onSubmit={e=>{
+                    e.preventDefault();
+                    createPatient({variables: {
+                        patientId: patientId.value,
+                        patientName: patientName.value,
+                        bodyTemperature: bodyTemperature.value,
+                        heartRate: heartRate.value,
+                        bloodPressure: bloodPressure.value,
+                        resporitoryRate: resporitoryRate.value,
+                        signsSymptoms: signsSymptoms.value
+                    }})
+                }}>
+                    <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Patient ID: </Form.Label>
+                        <Form.Control type="text" id="patientId" disabled value={props.data.patientId} name="patientId" ref={node => {patientId = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Patient Name: </Form.Label>
+                        <Form.Control type="text" id="patientName" disabled value={props.data.patientName} name="patientName" ref={node => {patientName = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Body Temperature: </Form.Label>
+                        <Form.Control type="number" id="bodyTemperature" value='0' name="bodyTemperature" ref={node => {bodyTemperature = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Heart Rate: </Form.Label>
+                        <Form.Control type="number" id="heartRate" value='0' name="heartRate" ref={node => {heartRate = node}} />
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Blood Pressure: </Form.Label>
+                        <Form.Control type="number" id="bloodPressure" value='0' name="bloodPressure" ref={node => {bloodPressure = node}} />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Resporitory Rate: </Form.Label>
+                        <Form.Control type="number" id="resporitoryRate" value='0' name="resporitoryRate" ref={node => {resporitoryRate = node}} />
+                    </Form.Group>
+                </Row>
+                    <Form.Group as={Col}>
+                    <Form.Label>Signs &#38; Symptoms: </Form.Label> 
+                    <Form.Check inline label="Runny nose" value='Runny nose' name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Sinus pain from congestion" value="Sinus pain from congestion" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Spots and blisters" value="Spots and blisters" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Poor wound healing" value="Poor wound healing"name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Thirst" value="Thirst" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Fast heart rate" value="Fast heart rate" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Chest Pain" value="Chest Pain" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Fever" value="Fever" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />
+                    <Form.Check inline label="Feelings of misery" value="Feelings of misery" name="signsSymptoms" type={'checkbox'} id="signsSymptoms" ref={node => {signsSymptoms = +node}} />    
+                    </Form.Group>
+                </Form>
+                
+            )}
+        </div>
+        
+    );
+}
+export default NewPatient;
